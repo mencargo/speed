d8b1f0d085ae5c2944624c098ff7173ca3b404dd
Martin <github@m-serve.de>
2016-10-22 15:21:16 +0200

    New example, "improved" garbage.php
    New example using justgage.js for nice displays, and Google Fonts for more awesomeness of this tool
    Changed garbage.php, now enforcing download (good for comparisons of measured speed vs. raw download speed) and disabling php buffering stuff

--- garbage.php
+++ garbage.php
@@ -1,9 +1,24 @@
 <?php
+// Disable Compression (would be too easy for 000...)
+@ini_set('zlib.output_compression', 'Off');
+@ini_set('output_buffering', 'Off');
+@ini_set('output_handler', '');
+// Headers
 header( "HTTP/1.1 200 OK" );
-$data=str_repeat("0",1048576);
+// Download follows...
+header('Content-Description: File Transfer');
+header('Content-Type: application/octet-stream');
+header('Content-Disposition: attachment; filename=random.dat'); 
+header('Content-Transfer-Encoding: binary');
+// Never cache me
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+// Generate data
+$data=str_repeat("0",1048575)."\n"; 
+// Deliver chunks of 1048576 bytes (or more - depending on encoding!)
 while(1){
-	echo $data;
-	flush();
+    echo $data;
+    flush();
 }
-
 ?>
