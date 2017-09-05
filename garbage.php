6730d2befc597cf239a2383b0ce7be00884936e0
dosse91 <dosse91@paranoici.org>
2017-09-05 07:45:52 +0200

    Fixed typo in garbage.php

--- garbage.php
+++ garbage.php
@@ -8,7 +8,7 @@ header('HTTP/1.1 200 OK');
 // Download follows...
 header('Content-Description: File Transfer');
 header('Content-Type: application/octet-stream');
-header('Content-Disposition: attachment; filename=random.dat'); 
+header('Content-Disposition: attachment; filename=random.dat');
 header('Content-Transfer-Encoding: binary');
 // Never cache me
 header('Cache-Control: no-store, no-cache, must-revalidate, max-age=0');
@@ -17,7 +17,7 @@ header('Pragma: no-cache');
 // Generate data
 $data=openssl_random_pseudo_bytes(1048576);
 // Deliver chunks of 1048576 bytes
-$chunks=isset($_GET['ckSize']) ?? intval($_GET['ckSize']) : 4;
+$chunks=isset($_GET['ckSize']) ? intval($_GET['ckSize']) : 4;
 if(empty($chunks)){$chunks = 4;}
 if($chunks>100){$chunks = 100;}
 for($i=0;$i<$chunks;$i++){
