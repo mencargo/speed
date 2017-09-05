2fda447449b9214b8cd720eb04653585f666fe4a
Slawka <slawka@eth0.org.ru>
2017-09-05 09:00:05 +0300

    New type of database sqlite (#58)
    The ability to select sqlite as a data source, it is desirable to put the catalog above the web or to forbid downloading
    ----------
    Возможность выбора sqlite в качестве бызы данных, желательно поместить каталогом выше веб или запретить скачивание

--- telemetry.php
+++ telemetry.php
@@ -1,8 +1,5 @@
 <?php
-	$MySql_username="USERNAME";
-	$MySql_password="PASSWORD";
-	$MySql_hostname="DB_HOSTNAME";
-	$MySql_databasename="DB_NAME";
+$db_type="sqlite"; //Type db mysql or sqlite
 
 $ip=($_SERVER['REMOTE_ADDR']);
 $ua=($_SERVER['HTTP_USER_AGENT']);
@@ -13,11 +10,42 @@ $ping=($_POST["ping"]);
 $jitter=($_POST["jitter"]);
 $log=($_POST["log"]);
 
-$conn = new mysqli($MySql_hostname, $MySql_username, $MySql_password, $MySql_databasename) or die("1");
-$stmt = $conn->prepare("INSERT INTO speedtest_users (ip,ua,lang,dl,ul,ping,jitter,log) VALUES (?,?,?,?,?,?,?,?)") or die("2");
-$stmt->bind_param("ssssssss",$ip,$ua,$lang,$dl,$ul,$ping,$jitter,$log) or die("3");
-$stmt->execute() or die("4");
-$stmt->close() or die("5");
-$conn->close() or die("6");
+if($db_type=="mysql"){
+        $MySql_username="USERNAME";
+        $MySql_password="PASSWORD";
+        $MySql_hostname="DB_HOSTNAME";
+        $MySql_databasename="DB_NAME";
 
+
+    $conn = new mysqli($MySql_hostname, $MySql_username, $MySql_password, $MySql_databasename) or die("1");
+    $stmt = $conn->prepare("INSERT INTO speedtest_users (ip,ua,lang,dl,ul,ping,jitter,log) VALUES (?,?,?,?,?,?,?,?)") or die("2");
+    $stmt->bind_param("ssssssss",$ip,$ua,$lang,$dl,$ul,$ping,$jitter,$log) or die("3");
+    $stmt->execute() or die("4");
+    $stmt->close() or die("5");
+    $conn->close() or die("6");
+
+}elseif($db_type=="sqlite"){
+
+    $file_db = "../telemetry.sql";
+
+    $conn = new PDO("sqlite:$file_db") or die("1");
+    $conn->exec("
+        CREATE TABLE IF NOT EXISTS `speedtest_users` (
+        `id`    INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+        `timestamp`     timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        `ip`    text NOT NULL,
+        `ua`    text NOT NULL,
+        `lang`  text NOT NULL,
+        `dl`    text,
+        `ul`    text,
+        `ping`  text,
+        `jitter`        text,
+        `log`   longtext
+        );
+    ");
+    $stmt = $conn->prepare("INSERT INTO speedtest_users (ip,ua,lang,dl,ul,ping,jitter,log) VALUES (?,?,?,?,?,?,?,?)") or die("2");
+    $stmt->execute(array($ip,$ua,$lang,$dl,$ul,$ping,$jitter,$log)) or die("3");
+    $conn = null;
+
+}
 ?>
