3c6857787688b2f837b315485ab392e92d4b973b
Brahm Lower <bplower@gmail.com>
2017-11-06 20:40:02 -0900

    Added postgresql db support to telemetry (#90)
    Added postgresql db support to telemetry

---
+++ telemetry_settings.php
@@ -0,0 +1,20 @@
+<?php
+
+$db_type="postgresql"; //Type of db: "mysql", "sqlite" or "postgresql"
+
+// Sqlite3 settings
+$Sqlite_db_file = "../telemetry.sql";
+
+// Mysql settings
+$MySql_username="USERNAME";
+$MySql_password="PASSWORD";
+$MySql_hostname="DB_HOSTNAME";
+$MySql_databasename="DB_NAME";
+
+// Postgresql settings
+$PostgreSql_username="USERNAME";
+$PostgreSql_password="PASSWORD";
+$PostgreSql_hostname="DB_HOSTNAME";
+$PostgreSql_databasename="DB_NAME";
+
+?>
