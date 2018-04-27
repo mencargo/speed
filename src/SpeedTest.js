df80817615914e507ce1bb02568fd25438ef7bc6
dunklesToast <tom.sacher@icloud.com>
2018-04-28 00:35:31 +0200

    Added ISP Detection

--- src/SpeedTest.js
+++ src/SpeedTest.js
@@ -2,6 +2,8 @@ const express = require('express');
 const Server = express();
 const randomBytes = require('random-bytes');
 const path = require('path');
+const request = require('request');
+const helpers = require('./Helpers');
 
 let cache = {
     size: 0,
@@ -45,7 +47,26 @@ Server.get('/garbage', function (req, res) {
 });
 
 Server.get('/getIP', function (req, res) {
-    res.send(req.headers['x-forwarded-for'] || req.connection.remoteAddress)
+    let requestIP = req.headers['x-forwarded-for'] || req.connection.remoteAddress || req.headers['HTTP_CLIENT_IP'] || req.headers['X-Real-IP'] || req.headers['HTTP_X_FORWARDED_FOR'];
+    if (requestIP.substr(0, 7) === "::ffff:") {
+        requestIP = requestIP.substr(7)
+    }
+    request('https://ipinfo.io/' + requestIP + '/json', function (err, body, ipData) {
+        ipData = JSON.parse(ipData);
+        if (err) res.send(requestIP);
+        else {
+            request('https://ipinfo.io/json', function (err, body, serverData) {
+                serverData = JSON.parse(serverData);
+                if (err) res.send(`${requestIP} - ${ipData.org}, ${ipData.country}`);
+                else if (ipData.loc && serverData.loc) {
+                    const d = helpers.calcDistance(ipData.loc.split(','), serverData.loc.split(','));
+                    res.send(`${requestIP} - ${ipData.org}, ${ipData.country} (${d}km)`);
+                } else {
+                    res.send(`${requestIP} - ${ipData.org}, ${ipData.country}`);
+                }
+            })
+        }
+    });
 });
 
 Server.use(express.static(path.join(__dirname, 'public')));
